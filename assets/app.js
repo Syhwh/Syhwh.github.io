f8bf6d44b368536214d35de6980109cbd50404d9
Shymmy W. Garcia <shymmywgarcia@gmail.com>
2018-11-24 07:25:21 -0500

    (Skills)

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particlesjs.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  
